c71c5c0d9ffc040d09ba0692531a6fad3ab9e972
Nekogeekaku <radric@wanadoo.fr>
2023-06-14 16:40:19 +0200

    brew clone

--- ec2_provisioning_with_terraform/main.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-access_key = "your access key"
-secret_key = "your secret key"
-region = "us-east-1"
-}
--- jenkins_by_vagrant/always_at_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Url"
-IP=$(ip -br addr  show |grep 192|egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}')
-echo "http://"$IP":8080/"
